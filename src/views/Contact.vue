52c298585ee8dee3ab7505520cd6406dc4af2f38
jhonathanmusa <jhonathansamu91@gmail.com>
2021-01-21 20:59:30 -0500

    adding contact form

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,6 +1,27 @@
 <template>
   <div class="app">
     <Header />
+    <h1 style="text-align: center">Contact Form</h1>
+    <form>
+      <p>
+        Name:
+        <input class="form-control" type="text" name="fullName" />
+      </p>
+      <p>
+        Email:
+        <input class="form-control" type="email" name="email" />
+      </p>
+      <p>
+        Message:
+        <textarea
+          class="form-control"
+          name="message"
+          cols="30"
+          rows="5"
+        ></textarea>
+      </p>
+      <button class="btn btn-success w-100">Send</button>
+    </form>
   </div>
 </template>
 
